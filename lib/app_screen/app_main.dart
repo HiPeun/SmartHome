dbb250666c86bd24e2afb07b587f04853142b9d2
charles <rmswo417@naver.com>
2024-05-22 17:43:58 +0900

    app_main.dart 76번줄에 AppMainView 커스텀 스테이트풀 생성 후 자동으로 넘어가는 이미지 생성함

--- lib/app_screen/app_main.dart
+++ lib/app_screen/app_main.dart
@@ -1,5 +1,3 @@
-import "dart:async";
-
 import "package:carousel_slider/carousel_slider.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
@@ -20,50 +18,59 @@ class AppMain extends StatefulWidget {
 class _AppMainState extends State<AppMain> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-            child: Row(
+            child: Column(
               children: [
-                Expanded(
-                  child: Text(
-                    "Conven",
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Conven",
+                        style: TextStyle(
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        "assets/images/backbutton.png",
-                        width: 26,
-                        height: 26,
-                        fit: BoxFit.cover,
+                    InkWell(
+                      onTap: () {},
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/images/backbutton.png",
+                            width: 26,
+                            height: 26,
+                            fit: BoxFit.cover,
+                          ),
+                          Text("로그인"),
+                        ],
                       ),
-                      Text("로그인"),
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        "assets/images/personbutton.png",
-                        width: 26,
-                        height: 26,
-                        fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/images/personbutton.png",
+                              width: 26,
+                              height: 26,
+                              fit: BoxFit.cover,
+                            ),
+                            Text("회원가입"),
+                          ],
+                        ),
                       ),
-                      Text("회원가입"),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+                AppMainView(),
               ],
             ),
           ),
@@ -91,31 +98,30 @@ class _AppMainViewState extends State<AppMainView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(20)),
-            CarouselSlider(
-              items: imgList
-                  .map(
-                    (e) => Container(
-                      child: Image.asset(
-                        e,
-                        fit: BoxFit.cover,
-                        width: 50,
-                      ),
+    return Container(
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.fromLTRB(0, 30, 0, 0)),
+          CarouselSlider(
+            items: imgList
+                .map(
+                  (e) => Container(
+                    child: Image.asset(
+                      e,
+                      fit: BoxFit.cover,
+                      width: 1000,
                     ),
-                  )
-                  .toList(),
-              options: CarouselOptions(
-                  autoPlay: true,  //자연스럽게 넘어 가는 기능
-                  aspectRatio: 2.0, //
-                  enlargeCenterPage: true,
                   ),
+                ).toList(),
+            options: CarouselOptions(
+              autoPlay: true, // 화면 전환을 자동으로 할건지 설정
+              aspectRatio: 1.6, //슬라이더가 화면의 비율에 맞춰지도록 합니다.
+              enlargeCenterPage: true,
+              viewportFraction: 1,
+              autoPlayInterval: Duration(seconds: 3) //화면 전환을 몇초마다 할건지 설정함
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
