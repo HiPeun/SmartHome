eec504fd4a9eca227193d55f62e346dcd8492ddb
charles <rmswo417@naver.com>
2024-05-22 15:26:41 +0900

    앱 화면 login 버튼 생성 로그인 앱 화면 회원가입 버튼 생성

--- lib/app_screen/app_main.dart
+++ lib/app_screen/app_main.dart
@@ -1,7 +1,8 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
-import 'package:flutter/material.dart';
-
-void main(){
+void main() {
   runApp(const AppMain());
 }
 
@@ -17,62 +18,49 @@ class _AppMainState extends State<AppMain> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(
-                  'Conven',
-                  style: TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
+                Expanded(
+                  child: Text(
+                    "Conven",
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "assets/images/backbutton.png",
+                        width: 26,
+                        height: 26,
+                        fit: BoxFit.cover,
+                      ),
+                      Text("로그인"),
+                    ],
                   ),
                 ),
 
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(150, 0, 0, 10),
-                  child: Row(
+                InkWell(
+                  onTap: () {},
+                  child: Column(
                     children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              'assets/images/backbutton.png',
-                              width: 26,
-                              height: 26,
-                              fit: BoxFit.cover,
-                            ),
-                            Text("로그인",)
-                          ],
-                        ),
+                      Image.asset(
+                        "assets/images/personbutton.png",
+                        width: 26,
+                        height: 26,
+                        fit: BoxFit.cover,
                       ),
+                      Text("회원가입"),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Row(
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              'assets/images/personbutton.png',
-                              width: 26,
-                              height: 26,
-                              fit: BoxFit.cover,
-                            ),
-                            Text("회원가입"),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                )
               ],
             ),
           ),
@@ -81,4 +69,3 @@ class _AppMainState extends State<AppMain> {
     );
   }
 }
-
