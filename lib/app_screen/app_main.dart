4233c35796dd6ee0bcd19244982cc151104a8933
charles <rmswo417@naver.com>
2024-05-22 16:40:29 +0900

    app_main.dart 76번줄에 AppMainView 커스텀 스테이트풀 생성 후 자동으로 넘어가는 이미 생성함

--- lib/app_screen/app_main.dart
+++ lib/app_screen/app_main.dart
@@ -1,5 +1,9 @@
+import "dart:async";
+
+import "package:carousel_slider/carousel_slider.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter/widgets.dart";
 
 void main() {
@@ -46,7 +50,6 @@ class _AppMainState extends State<AppMain> {
                     ],
                   ),
                 ),
-
                 InkWell(
                   onTap: () {},
                   child: Column(
@@ -60,10 +63,58 @@ class _AppMainState extends State<AppMain> {
                       Text("회원가입"),
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppMainView extends StatefulWidget {
+  const AppMainView({super.key});
+
+  @override
+  State<AppMainView> createState() => _AppMainViewState();
+}
+
+class _AppMainViewState extends State<AppMainView> {
+  // 이미지 슬라이더 list로 묶음
+  final List<String> imgList = [
+    'assets/webmain/webmain1.png',
+    'assets/webmain/webmain2.png',
+    'assets/webmain/webmain3.png',
+    'assets/webmain/webmain4.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(20)),
+            CarouselSlider(
+              items: imgList
+                  .map(
+                    (e) => Container(
+                      child: Image.asset(
+                        e,
+                        fit: BoxFit.cover,
+                        width: 50,
+                      ),
+                    ),
+                  )
+                  .toList(),
+              options: CarouselOptions(
+                  autoPlay: true,  //자연스럽게 넘어 가는 기능
+                  aspectRatio: 2.0, //
+                  enlargeCenterPage: true,
+                  ),
+            ),
+          ],
         ),
       ),
     );
