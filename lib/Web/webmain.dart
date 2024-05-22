b58058ebbd349203333b73e469da15daf25c9e52
jiyun <ljy3852@naver.com>
2024-05-22 16:06:57 +0900

    웹 main 상단바 로고,로그인,회원가입,고객센터 버튼 만듬.
    슬라이더 플러그인 사용후 작동확인
    아직 수정중~

--- lib/Web/webmain.dart
+++ lib/Web/webmain.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -32,13 +33,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // 이미지 슬라이더 list로 묶음
+  final List<String> imgList = [
+    'assets/webmain/webmain1.png',
+    'assets/webmain/webmain2.png',
+    'assets/webmain/webmain3.png',
+    'assets/webmain/webmain4.png',
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +55,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Row(
                   children: [
                     Expanded(
-                      flex: 4,
                       child: Text(
                         "Conven",
                         style: TextStyle(
@@ -106,7 +107,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-              )
+              ),
+              // 이미지 슬라이더
+              CarouselSlider.builder(
+                itemCount: imgList.length,
+                itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+                     Container(
+                    child: Image.network(imgList[itemIndex], fit: BoxFit.cover),
+                  ),
+                options: CarouselOptions(
+                  height: 400.0, // 슬라이더의 높이를 지정
+                  aspectRatio: 16 / 9, // 슬라이더의 종횡비를 지정
+                  initialPage: 0, // 처음에 표시될 슬라이드의 인덱스
+                  enableInfiniteScroll: true, // 무한 스크롤 활성화
+                  reverse: false, // 슬라이드 넘기는 방향 반전
+                  autoPlay: true, // 자동 재생 활성화
+                  autoPlayInterval: Duration(seconds: 5), // 자동 재생 간격
+                  autoPlayCurve: Curves.fastOutSlowIn, // 자동 재생 애니메이션 커브
+                  pauseAutoPlayOnTouch: true, // 사용자가 터치하면 자동 재생 일시 중지
+                  onPageChanged: (index, reason) { // 페이지가 변경될 때 실행할 함수
+                  print('Page changed: $index, Reason: $reason');
+            },
+            scrollDirection: Axis.horizontal, // 스크롤 방향을 가로로 설정
+          ),)
             ],
           ),
         ),
