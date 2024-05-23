5f2478b66048faee774351e9737ffe6517428a84
kimji-yu <qkrwldb77@naver.com>
2024-05-23 16:08:03 +0900

    회원가입 UI화면 수정

--- lib/Web/Web_Member/web_join.dart
+++ lib/Web/Web_Member/web_join.dart
@@ -17,6 +17,16 @@ class _WebJoinState extends State<WebJoin> {
   TextEditingController id = TextEditingController();
 
   bool isAgreed = false;
+  bool isPersonAgreed = false;
+  bool isTermsAgreed = false;
+
+  void toggleAgreement(bool value) {
+    setState(() {
+      isAgreed = value;
+      isPersonAgreed = value;
+      isTermsAgreed = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -103,12 +113,16 @@ class _WebJoinState extends State<WebJoin> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 400, right: 400, top: 100),
+            SizedBox(
+              height: 50,
+            ),
+
+            Center(
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextField(
+                children: [
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
                       controller: name,
                       decoration: InputDecoration(
                         prefixIcon: Icon(Icons.person),
@@ -118,87 +132,73 @@ class _WebJoinState extends State<WebJoin> {
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: TextField(
-                        controller: id,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.key),
-                          labelText: '아이디',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                      controller: id,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.key),
+                        labelText: '아이디',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: TextField(
-                        controller: pw,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            labelText: "비밀번호",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                      controller: pw,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: "비밀번호",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: TextField(
-                        controller: pw2,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          labelText: "비밀번호 확인",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                      controller: pw2,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: "비밀번호 확인",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: TextField(
-                        controller: email,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.markunread_rounded),
-                          labelText: "이메일",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                      controller: email,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.markunread_rounded),
+                        labelText: "이메일",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: isAgreed,
-                                onChanged: (value) {
-                                  setState(() {
-                                    isAgreed = value!;
-                                  });
-                                },
-                              ),
-                              Text(
-                                "전체 동의",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              // 약관 동의 페이지로 이동
-                            },
+                  ),
+                  SizedBox(height: 16),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: TextButton(
+                            onPressed: () {},
                             child: Text(
                               "약관 동의",
                               style: TextStyle(
@@ -207,33 +207,102 @@ class _WebJoinState extends State<WebJoin> {
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Checkbox(
+                          value: isAgreed,
+                          onChanged: (value) {
+                            toggleAgreement(value!);
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 320),
+                          child: Text(
+                            "전체 동의",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40),
-                      child: ElevatedButton(
-                        onPressed: () {
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: isPersonAgreed,
+                        onChanged: (value) {
+                          setState(() {
+                            isPersonAgreed = value!;
+                            if (!value) {
+                              isAgreed = false;
+                            } else if (isTermsAgreed) {
+                              isAgreed = true;
+                            }
+                          });
                         },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFD3CDC8),
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          fixedSize: Size.fromHeight(45),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 310),
                         child: Text(
-                          "회원가입",
+                          "[필수] 개인 정보 수집",
                           style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: isTermsAgreed,
+                        onChanged: (value) {
+                          setState(() {
+                            isTermsAgreed = value!;
+                            if (!value) {
+                              isAgreed = false;
+                            } else if (isPersonAgreed) {
+                              isAgreed = true;
+                            }
+                          });
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 350),
+                        child: Text(
+                          "[필수] 이용약관",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 40),
+                  SizedBox(
+                    width: 500,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFD3CDC8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "회원가입",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ],
+                  ),
+                ],
               ),
             ),
           ],
