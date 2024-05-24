142bd5d6b652a9e7678dbc00a312bfa4f0590c56
charles <rmswo417@naver.com>
2024-05-24 12:27:33 +0900

    컨테이너에 상자 두개 추가 컨테이너에 검색어 입력창 추가 컨테이너에 검은색 상자 추가

--- lib/app_screen/app_page1.dart
+++ lib/app_screen/app_page1.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Page1 extends StatelessWidget {
   @override
@@ -18,29 +20,70 @@ class Page1 extends StatelessWidget {
           ],
         ),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Color(0xFFE5E5E1),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Color(0xFFE5E5E1),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 400,
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SearchBar(
+                    trailing: [
+                      IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () {},
+                      ),
+                    ],
+                    constraints: BoxConstraints(maxHeight: 100, maxWidth: 250),
+                    shape: MaterialStateProperty.all(
+                      ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    hintText: "검색어를 입력하세요",
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 300,
+              color: Colors.black,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 5,
+                    color: Color(0xFFE5E5E1),
+                  )
+                ],
 
-        children: [
-          Container(
-            margin: EdgeInsets.all(20),
-            width: 140,
-            height: 140,
-            color: Color(0xFFE5E5E1),
-            child: Image.asset("assets/images/speakerimage.png"),
-          ),
+              ),
 
-          Container(
-            margin: EdgeInsets.all(20),
-            width: 140,
-            height: 140,
-            color: Color(0xFFE5E5E1),
-            child: Image.asset("assets/images/noteimage.png"),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
-
     );
   }
 }
-
