63a282f04f7eed15e7b70be748dcd740215a80f8
kimji-yu <qkrwldb77@naver.com>
2024-06-12 15:13:29 +0900

    새로운 비밀번호 만들기 API 받는작업중

--- lib/Web/webmain.dart
+++ lib/Web/webmain.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:loginproject/Web/Web_Cus/web_notice.dart';
 import 'package:loginproject/Web/Web_Member/web_join.dart';
 import 'package:loginproject/Web/Web_Member/web_login.dart';
-import 'package:loginproject/Web/Web_Member/web_login_screen.dart';
 import 'package:loginproject/Web/Web_Member/web_modify_profile.dart';
 
 import '../App/main.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
