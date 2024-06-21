d3773810c20f75a9fdac33104aace3f6d104f999
jiyun <ljy3852@naver.com>
2024-06-21 15:40:07 +0900

    LED,온습도,창문,현관문개폐 완료 CCTV 거의 다 됌, 불꽃감지센서 하는 중

--- lib/App/app_screen/app_page2.dart
+++ lib/App/app_screen/app_page2.dart
@@ -1,8 +1,11 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:loginproject/App/Arduino/cctv.dart';
 import 'package:loginproject/main.dart';
+import '../../main.dart';
+import '../../main.dart';
 import 'app_join.dart';
 import 'app_login.dart';
 import 'package:dio/dio.dart';
@@ -30,6 +33,8 @@ class _Page2State extends State<Page2> {
       sendTimeout: Duration(seconds: 10),
     ),
   );
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
 
   @override
   void initState() {
@@ -152,6 +157,100 @@ class _Page2State extends State<Page2> {
     }
   }
 
+  // 불꽃 감지 센서
+  Future<void> checkFlameStatus() async {
+    String url = 'http://192.168.0.231/checkFlame';
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        print('HTTP Response Status Code: ${response.statusCode}');
+        String status = response.body.trim();
+        print('HTTP Response Body: "$status"'); // 응답 본문 출력
+        if (status == 'Danger') {
+          print('불꽃 감지 되었습니다.');
+          showDangerAlert();
+          sendEmergencyNotification();
+        } else {
+          print('안전 상태로 판단됨.');
+          showSafeAlert();
+        }
+      } else {
+        print('Failed to check flame status. Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Failed to check flame status. Exception: $e');
+    }
+  }
+
+  void showDangerAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('위험 감지!'),
+          content: Text('불이 감지되었습니다. 필요한 조치를 취하세요!'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('닫기'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showSafeAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('안전 상태'),
+          content: Text('불이 감지되지 않았습니다. 현재 상황은 안전합니다.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('닫기'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> sendEmergencyNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'emergency_channel_id',
+      '비상 알림',
+      channelDescription: '비상 상황을 위한 알림',
+      importance: Importance.max,
+      priority: Priority.high,
+      enableLights: true,
+      enableVibration: true,
+      playSound: true,
+      sound: RawResourceAndroidNotificationSound('emergency_sound'),
+      fullScreenIntent: true,
+    );
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      '비상!',
+      '불이 감지되었습니다. 긴급 상황입니다. 비상 서비스에 연락하세요.',
+      platformChannelSpecifics,
+      payload: 'emergency',
+    );
+  }
+
+
+
 
 
   // 로그인 후
@@ -177,8 +276,21 @@ class _Page2State extends State<Page2> {
 
 
 
+
+
+
+
   @override
   Widget build(BuildContext context) {
+    // 알림 설정 초기화
+    var initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+
     return Material(
       child: SafeArea(
         child: SingleChildScrollView(
@@ -384,6 +496,7 @@ class _Page2State extends State<Page2> {
                     iconButton: IconButton(
                       onPressed: () {
                         if (user.isNotEmpty) {
+                          checkFlameStatus();
                         } else {
                           showLoginAlert(context);
                         }
